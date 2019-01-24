c219f0c51f2e079b738af40ca4c681a69252d9b8
BeatrizMT <44165915+BeatrizMT@users.noreply.github.com>
2019-01-24 15:38:10 +0100

    Update scrape_vikidia.py

--- spikes/scrape_vikidia.py
+++ spikes/scrape_vikidia.py
@@ -4,77 +4,78 @@ import copy
 import re
 import datetime
 import random
-import textstat
 
-""" recupere 1000 articles aleatoirement de vikidia et retourne TOUT ce qu'il trouve au format html (option texte seulement mise 
-en commentaire meme si elle renvoie des parties du texte pas pertinentes)"""
+
+""" constitution du corpus : articles a simplifier, super articles et de l'aléatoire """
 
 random.seed(datetime.datetime.now())
-def getLinks(articleUrl):
+def getLinks(articleUrl,write=""):
 
-    req = Request("https://fr.vikidia.org" + articleUrl, headers={'User-Agent': 'Mozilla/5.0'})
-    webpage = urlopen(req)
-    bsObj = BeautifulSoup(webpage,"lxml")
-    print("<article title="+articleUrl+'" format=\"html\">')
-    #print(str(bsObj.find("div", {"id": "bodyContent"})))
-    #print(bsObj.find("div", {"id": "bodyContent"}).get_text()) ## pour n'avoir que le texte
-    #print("</article>")
+	req = Request("https://fr.vikidia.org" + articleUrl, headers={"User-Agent":'Mozilla/5.0'})
+	webpage = urlopen(req)
+	bsObj = BeautifulSoup(webpage,"lxml")
+	if write != "": 
+		print("<article title=\"" + articleUrl + '" type=\"' + str(write) +'">')
+		part = bsObj.find("div", {"id" : "mw-content-text"})
 
-    part = bsObj.find("div", {"id" : "mw-content-text"})
+		for div in part.findAll("div"): 
+			div.decompose()
 
-    for div in part.findAll("div"): 
-        div.decompose()
+		for center in part.findAll("center"):
+			center.decompose()
 
-    for center in part.findAll("center"):
-        center.decompose()
+		for table in part.findAll("table"):
+			table.decompose()
 
-    for table in part.findAll("table"):
-        table.decompose()
+		for child in part.children:
+			if isinstance(child, Comment):
+				child.extract()
 
-    for child in part.children:
-        if isinstance(child, Comment):
-            child.extract()
+		def replace_by_text(part, tag):
+			for child in part.findAll(tag):
+				child.insert_before(child.text)
+				child.decompose()
+			return part
 
-    def replace_by_text(part, tag):
-        for child in part.findAll(tag):
-            child.insert_before(child.text)
-            child.decompose()
-        return part
+		replace_by_text(part, "a")
+		replace_by_text(part, "b")
+		replace_by_text(part, "i")
+		replace_by_text(part, "strong")
+		replace_by_text(part, "em")
+		replace_by_text(part, "u")
+		text = ''
+		for child in part.findAll("p"):
+			text += child.text
 
-    replace_by_text(part, "a")
-    replace_by_text(part, "b")
-    replace_by_text(part, "i")
-    replace_by_text(part, "strong")
-    replace_by_text(part, "em")
-    replace_by_text(part, "u")
+		print(text)
+		print("</article>")
+		
+	# webpage = urlopen(req)
+	# bsObj = BeautifulSoup(webpage,"lxml")
+	links = bsObj.find("div", {"id" : "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
 
-    text = ''
-    for child in part.findAll("p"):
-        text += child.text
-
-    print(text)
-    print('----')
-    print(textstat.flesch_reading_ease(text))
-    
-    webpage = urlopen(req)
-    bsObj = BeautifulSoup(webpage,"lxml")
-    links = bsObj.find("div", {"id" : "bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
-    
-    return links
+	return links
 
 #html = urlopen("https://fr.vikidia.org/wiki/Arbre")
 
 
 if __name__ =="__main__":
-    print("<corpus>")
-    links = getLinks("/wiki/Arbre")
-    pages = set("/wiki/Arbre")
-    i = 0
-    while len(links) > 0 and i < 1000:
-        newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
-        if newArticle not in pages:
-            pages. add(newArticle)
-            links = getLinks(newArticle)
-            i += 1
-
-    print("</corpus>")
+	print("<corpus>")
+	pages = set()
+	links = getLinks("/wiki/Vikidia:Super_article/Promus","")
+	for link in links:
+		getLinks(link,"super")
+    
+  
+    
+    # i = 0
+    # links = getLinks("/wiki/Arbre")
+    # pages.add("/wiki/Arbre")
+    # while len(links) > 0 and i < 2000:
+        # newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+        # if newArticle not in pages:
+            # pages.add(newArticle)
+            # links = getLinks(newArticle)
+            # i += 1
+	print("</corpus>")
+	
