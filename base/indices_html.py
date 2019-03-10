50943e2312fb41b63edf43353a24ae917f26667c
BeatrizMT <44165915+BeatrizMT@users.noreply.github.com>
2019-03-10 12:58:59 +0100

    Update indices_html.py
    integration indices ponctuation

--- base/indices_html.py
+++ base/indices_html.py
@@ -40,6 +40,50 @@ def reperage_images_liens_viki(url):
 			"STRUCTURE" : structure
 	}
 
+def reperage_ponctuation(url):
+	req = Request("https://fr.vikidia.org/wiki/" + url, data = None, headers={"User-Agent":'Mozilla/5.0'})
+	webpage = urlopen(req)
+	html_doc = webpage.read()
+	soup = BeautifulSoup(html_doc,"html.parser")
+	gras=soup.find_all('b')
+	scoreHTML= 0
+	ponctuation=[".","!","?","...",";",":","\"","(",")","[","]", "/", "«", "»", "{","}"]
+	typographie=["n°","&","%","$","£","€","#","§","@", "+","=","\*", "<",">"]
+	if gras:
+		scoreHTML+=(len(gras))
+	italique=soup.find_all('i')
+	if italique:
+		scoreHTML+=(len(italique))
+	souligne=soup.find_all('u')
+	if souligne:
+		scoreHTML+=(len(souligne))
+
+	visible_text = soup.find_all('p')
+	for element in visible_text:
+		contenu=str(element.text)
+		#print(contenu)
+		nombre = re.finditer("[0-9]+(,[0-9]+)?", contenu)
+		finAlinea= re.finditer("\r", contenu) # je ne suis pas sûre, l'auteur parle de fin d'alinéa donc je comprend ça comme un retour à la ligne, ajouter un \n?
+		debutAlinea=re.finditer("^\t",contenu)
+		if nombre: #je ne boucle pas pour les nombres afin de prendre en compte les nombres décimaux
+			scoreHTML+=1
+		if finAlinea:
+			scoreHTML+=1
+		if debutAlinea:
+			scoreHTML+=1
+		for caractere in contenu:
+			majuscule = re.search("[A-Z]", caractere)
+			if majuscule:
+				scoreHTML+=1
+			if caractere in ponctuation:
+				scoreHTML+=1
+			if caractere in typographie:
+				scoreHTML+=1
+
+	print("Score HTML", scoreHTML) # à normaliser avec le nombre de tokens
+	return { "META_HTML_score" : scoreHTML}
+
+
 if __name__== "__main__":
 	#dossier = sys.argv[1]
 	url = "Navette_spatiale_Bourane"
